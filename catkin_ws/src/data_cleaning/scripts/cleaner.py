d31aa6808c978747c0e84e765021d2c76ada9295
Janelle Blankenburg <jjblankenburg@gmail.com>
2016-04-17 20:14:03 -0700

    Got time stuff working. Next to make it so can pass in file names and then put everything in a launch file to run all the things and then run it on all the files yay!

--- catkin_ws/src/data_cleaning/scripts/cleaner.py
+++ catkin_ws/src/data_cleaning/scripts/cleaner.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 import rospy
+import yaml
+import rosbag
 import message_filters
 import sensor_msgs.msg
 import geometry_msgs.msg
 import ardrone_autonomy.msg
 import rosgraph_msgs.msg
 import std_msgs.msg
-import data_cleaning.msg
+# import data_cleaning.msg
 
 
 # global stuff to fix later
 msg = geometry_msgs.msg.Twist()
 count = 0
 
-fname = open('data_cleaned.txt', 'w')
+# fname = open('data_cleaned.txt', 'w')
 
  # define listener for synchronizing data
-def listener():
+def listener(data_file_name, stop_time):
     rospy.init_node('cleaner')
     sub =[]
     sub.append(message_filters.Subscriber('/ardrone/image_raw', sensor_msgs.msg.Image))
@@ -31,11 +33,27 @@ def listener():
 def callback(img,nav):
     global count
     global msg
-    global fname
+    global data_file_name
+    global end_time
+
+    # global fname
     # print(nav,msg)
     # print('count!!!!:', count)
     # print('\n\n\n\n\n\n\n\n')
     # count = count + 1
+
+    # get time current time stamp
+    time = rospy.get_time()
+    # time = nav
+    print(time, end_time, end_time - time)
+
+    # if time >= stop_time shut down
+    if end_time - time <= 0.4:
+
+    #     # shut down this node?
+        rospy.signal_shutdown("DONE!!!") 
+        # rospy.shutdown("DONE!!!") 
+
 
     # save data to correct format ----- what format is this?!?!?
     data = {'img': img, 'vel':msg, 'nav': nav}
@@ -46,12 +64,12 @@ def callback(img,nav):
     # print('\n\n\n\n\n\n\n\n')
     # count = count + 1
 
-    fname.write(str(data['nav']));
-    fname.write('\n;\n');
-    fname.write(str(data['vel']));
-    fname.write('\n;\n');
-    fname.write(str(data['img']));
-    fname.write('\n;\n');
+    data_file_name.write(str(data['nav']));
+    data_file_name.write('\n;\n');
+    data_file_name.write(str(data['vel']));
+    data_file_name.write('\n;\n');
+    data_file_name.write(str(data['img']));
+    data_file_name.write('\n;\n');
 
 
 
@@ -62,9 +80,28 @@ def callback2(msg_t):
 
 # Main
 if __name__ == '__main__':
-    listener()
+
+    global data_file_name
+    global end_time
+
+    # open bag file 
+    bag_file_name = '/home/janelle/Documents/thesis/catkin_ws/src/data_cleaning/rosbag/bad/FlightBackward2.bag'
+
+    # get start time, end time, and duration
+    info_dict = yaml.load(rosbag.Bag(bag_file_name, 'r')._get_yaml_info())
+    start_time = info_dict['start']
+    end_time = info_dict['end']
+    duration = info_dict['duration']
+
+    print(start_time, end_time, duration)
+
+    # read in and write data from bag file
+    data_file_name = open('data_cleaned.txt', 'w')
+    listener(data_file_name, end_time)
+
+    # close file and finish
     print('THIS FINISHED????!!!')
-    fname.close()
+    data_file_name.close()
 
 
 # 205099-202513 = 2586            bag time      rate
